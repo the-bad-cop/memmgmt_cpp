1210547f875e17865b89b9babd50e8cc3545dc0c
fear <hpneedforspeed2728@gmail.com>
2020-05-16 22:56:33 +0530

    smart

--- raii/smart2.cpp
+++ raii/smart2.cpp
@@ -8,7 +8,7 @@ class MyClass
 public:
     MyClass() {}
     MyClass(std::string str) : _text(str) {}
-    ~MyClass() { std::cout << "Text destroyed\n "; }
+    ~MyClass() { std::cout << _text << "Text destroyed\n "; }
     void setText(std::string text) { _text = text; }
 };
 
@@ -17,7 +17,9 @@ int main()
     std::unique_ptr<MyClass> obj1(new MyClass());
     std::unique_ptr<MyClass> obj2(new MyClass("Hello"));
     obj1->setText("World");
-    *obj2 = *obj1;
+    *obj2 = *obj1; //even after copying both have different stack adresses coz heap data is only copied
+
+    std::unique_ptr<MyClass> obj3 = obj1;
 
     //use the .get() fn to het the address of the raw pointer. ie; the obj handle ptr exists in the stack mem
     //to get the adress of the object alocated in the heap, .get() is used
