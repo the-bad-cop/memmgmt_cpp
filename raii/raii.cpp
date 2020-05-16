0321c6f6aaadd121be8d388990f8505710864231
fear <hpneedforspeed2728@gmail.com>
2020-05-16 22:33:14 +0530

    smart

--- raii/raii.cpp
+++ raii/raii.cpp
@@ -20,8 +20,9 @@ int main()
     for (size_t i = 0; i < 5; i++)
     {
         // int *e = new int(i);
-        raiClass e(new int(8));
-        std::cout << "Dividing " << *e << " with " << a[i] << " = " << *e / a[i] << std::endl;
+        raiClass e(new int(i)); //when e goes of of scope, it will be automatically deallocated
+        //also here allocation is done within the main class and the pointer is copied to the class
+        std::cout << "Dividing " << *e << " with " << a[i] << " = " << (float)*e / a[i] << std::endl;
         // delete e;
     }
 }
---
+++ raii/smart2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <memory>
+class MyClass
+{
+    std::string _text;
+
+public:
+    MyClass() {}
+    MyClass(std::string str) : _text(str) {}
+    ~MyClass() { std::cout << "Text destroyed\n "; }
+    void setText(std::string text) { _text = text; }
+};
+
+int main()
+{
+    std::unique_ptr<MyClass> obj1(new MyClass());
+    std::unique_ptr<MyClass> obj2(new MyClass("Hello"));
+    obj1->setText("World");
+    *obj2 = *obj1;
+
+    //use the .get() fn to het the address of the raw pointer. ie; the obj handle ptr exists in the stack mem
+    //to get the adress of the object alocated in the heap, .get() is used
+    std::cout << "Address of obj1 in stack is " << &obj1 << " and heap is " << obj1.get() << std::endl;
+}
